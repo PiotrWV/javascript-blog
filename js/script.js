43fa11c79a04a18561e5bb1e5b6dd3ea0417db9a
Piotr <piotr.wach122@gmail.com>
2020-02-01 22:30:26 +0100

    Add function that remove article title after click

--- js/script.js
+++ js/script.js
@@ -7,7 +7,22 @@ const titleClickHandler = function () {
 
     /* remove class 'active' from all article links  */
 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+
+        activeLink.classList.remove('active');
+
+    }
+
     /* add class 'active' to the clicked link */
+
+    const activeArticles = document.querySelectorAll('article a.active');
+
+    for (let activeArticle of activeArticles) {
+
+        activeArticle.classList.remove('active');
+    }
 
     /* remove class 'active' from all articles */
 
